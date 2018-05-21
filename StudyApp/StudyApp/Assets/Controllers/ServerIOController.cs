1f201292464eae3a965d90a2004ad551259f8d9c
Alex Baatz <abaatz@student.neumont.edu>
2018-05-21 14:53:44 -0600

    Added methods for passing new objects to the server and deleting objects.

--- StudyApp/StudyApp/Assets/Controllers/ServerIOController.cs
+++ StudyApp/StudyApp/Assets/Controllers/ServerIOController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Json;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,27 +13,71 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
-using Java.IO;
+using Newtonsoft.Json;
+using StudyApp.Assets.Models;
+using File = StudyApp.Assets.Models.File;
 
 namespace StudyApp.Assets.Controllers
 {
     class ServerIOController
     {
         public static readonly string IP = "104.42.173.109";
+
+        public void DeleteFile(string guid)
+        {
+            PassToServer(guid, "file", "DeleteFile");
+
+        }
+        public void DeleteNote(string guid)
+        {
+            PassToServer(guid, "note", "DeleteNote");
+        }
+
+        public void CreateNote(Note note)
+        {
+            string json = "";
+            JsonSerializer serializer = JsonSerializer.Create();
+            serializer.Serialize(new StringWriter(new StringBuilder(json)),note);
+
+            PassToServer(json, "note", "CreateNote");
+        }
+
+        public void UploadFile(File file)
+        {
+            string json = "";
+            JsonSerializer serializer = JsonSerializer.Create();
+            serializer.Serialize(new StringWriter(new StringBuilder(json)), file);
+
+            PassToServer(json, "file", "UploadFile");
+        }
+
+        public void CreateGoal(Goal goal)
+        {
+            string json = "";
+            JsonSerializer serializer = JsonSerializer.Create();
+            serializer.Serialize(new StringWriter(new StringBuilder(json)), goal);
+
+            PassToServer(json, "goal", "CreateGoal");
+        }
+
+        private static async void PassToServer(string content, string modelType, string controller)
+        {
+            string url = $"https://{IP}/{modelType}/{controller}/{content}";
+            HttpClient client = new HttpClient();
+            await client.PostAsync(url, null);
+        }
+
+
+
         //TODO: I DON'T KNOW IF THIS WILL WORK. PLEASE TEST.
         public async Task<bool> AuthenticateUser(string username, string password)
         {
-            string url = $"https://{IP}/AuthenticateUser/username={username}&password={password}";
+            string url = $"https://{IP}/user/AuthenticateUser/username={username}&password={password}";
             HttpClient client = new HttpClient();
             var response = await client.PostAsync(url, null);
             Task<string> thing = response.Content.ReadAsStringAsync();
-            JsonReader reader = new JsonReader(new StringReader(thing.Result));
-            bool val = false;
-            while (reader.HasNext)
-            {
-                val = reader.NextBoolean();
-            }
-            return val;
+            Newtonsoft.Json.JsonReader reader = new JsonTextReader(new StringReader(thing.Result));
+            return reader.ReadAsBoolean() ?? false;
         }
     }
 }
