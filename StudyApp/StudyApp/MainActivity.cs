d0492bfb5b42447237f3b2721ac5caa6739431a3
priyaVora <prvora89@gmail.com>
2018-05-29 00:08:47 -0600

    Added a temporary App Icon

--- StudyApp/StudyApp/MainActivity.cs
+++ StudyApp/StudyApp/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace StudyApp
 {
-    [Activity(Label = "StudyApp", MainLauncher = true)]
+    [Activity(Label = "StudyApp", MainLauncher = true, Icon ="@drawable/Envision")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
