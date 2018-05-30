cc55145bf25c0ec616c7794e9168d29c10024a5a
Unknown <prvora89@gmail.com>
2018-05-30 13:42:06 -0600

    Working on adding RecyclerView to File Activity - Unfinished Code

--- StudyApp/StudyApp/Assets/Views/FileViewActivity.cs
+++ StudyApp/StudyApp/Assets/Views/FileViewActivity.cs
@@ -7,18 +7,43 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
 
 namespace StudyApp.Assets.Views {
 
     [Activity(Label = "FileViewActivity")]
-    public class FileViewActivity : Activity {
+    public class FileViewActivity : CommonActivity {
+        RecyclerView mRecyclerView;
+        RecyclerView.LayoutManager mLayoutManager;
 
-        protected override void OnCreate(Bundle savedInstanceState) {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.FilePage);
-            // Create your application here
+
+            /*
+             * This code is how to replace the placeholder layout that's part of the CommonLayout.
+             */
+            FrameLayout frame = FindViewById<FrameLayout>(Resource.Id.Common_FrameLayout);
+            View file = LayoutInflater.Inflate(Resource.Layout.FilePage, null); // Replace the inside of this method call with your desired layout
+            frame.AddView(file.FindViewById<LinearLayout>(Resource.Id.File_Layout));
+
+            /*
+             * This method needs to be called on the OnCreate method for any activities inheriting from CommonActivity,
+             * since this is what initializes the navbar
+             */
+
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            mLayoutManager = new LinearLayoutManager(this);
+            mRecyclerView.SetLayoutManager(mLayoutManager);
+
+
+
+
+            SetUpNavBar();
+
+            
         }
 
         public void UploadClick(object sender, EventArgs args) {
---
+++ StudyApp/StudyApp/RecyclerView_Resources/FileViewHolder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace StudyApp.RecyclerView_Resources
+{
+    public class FileViewHolder : RecyclerView.ViewHolder
+    {
+        public TextView Caption { get; private set; }
+
+        public FileViewHolder(View itemView) : base(itemView)
+        {
+            Caption = itemView.FindViewById<TextView>(Resource.Id.textView);
+        }
+    }
+}
