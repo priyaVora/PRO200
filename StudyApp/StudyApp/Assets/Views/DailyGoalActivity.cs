0fdf170ce3143b1d71cc220d6d94ba3d2f8f7ca8
Unknown <abaatz@student.neumont.edu>
2018-06-04 14:40:49 -0600

    Working on daily goals

--- StudyApp/StudyApp/Assets/Views/DailyGoalActivity.cs
+++ StudyApp/StudyApp/Assets/Views/DailyGoalActivity.cs
@@ -7,24 +7,44 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
 using StudyApp.Assets.Models;
+using StudyApp.RecyclerView_Resources;
 
 namespace StudyApp.Assets.Views {
     [Activity(Label = "DailyGoalActivity")]
-    public class DailyGoalActivity : CommonActivity {
+    public class DailyGoalActivity : CommonActivity
+    {
+        private RecyclerView mRecyclerView;
+        private RecyclerView.LayoutManager mLayoutManagerRecycler;
+        private GoalAdapter mAdapter;
+        private GoalAlbum mGoalAlbum;
         protected override void OnCreate(Bundle savedInstanceState) {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
 
-            List<RecurringGoal> recurringGoals = goalController
-                .GetUpcomingRecurringGoals(userController.CurrentUser.UserName).Where(g => g.Deadline == DateTime.Today)
-                .ToList();
-            List<NonRecurringGoal> nonRecurringGoals = goalController.GetUpcomingNonRecurringGoals(userController.CurrentUser.UserName).Where(g => g.Deadline == DateTime.Today)
-                .ToList();
+            FindViewById<TextView>(Resource.Id.DailyGoals_DayLabel).Text = DateTime.Now.Day.ToString();
+            FindViewById<TextView>(Resource.Id.DailyGoals_DayOfWeekLabel).Text = DateTime.Now.DayOfWeek.ToString();
+            mRecyclerView = FindViewById<RecyclerView>(Resource.Id.DailyGoals_RecyclerView);
+            mLayoutManagerRecycler = new LinearLayoutManager(this);
+            mRecyclerView.SetLayoutManager(mLayoutManagerRecycler);
+            SetGoalData();
+            mAdapter = new GoalAdapter(mGoalAlbum);
+            mRecyclerView.SetAdapter(mAdapter);
 
+        }
+
+        private void SetGoalData()
+        {
+            List<Goal> temp = new List<Goal>();
+
+            goalController.GetUpcomingRecurringGoals(userController.CurrentUser.UserName).Where(g => g.Deadline == DateTime.Today).ToList().ForEach(temp.Add);
+            goalController.GetUpcomingNonRecurringGoals(userController.CurrentUser.UserName).Where(g => g.Deadline == DateTime.Today).ToList().ForEach(temp.Add);
+
+            mGoalAlbum = new GoalAlbum(temp);
         }
 
         public void OnTouchEvent(object sender, EventArgs args) {
