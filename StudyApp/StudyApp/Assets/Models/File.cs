f04c5c2b40fd83ad51fd0cffe6de14e1910372d1
Ramon Caballero <ramon.caballeroiv@gmail.com>
2018-06-04 19:43:01 -0600

    Added an implicit conversion from File to FileMini.
    Added Size property to FileMini.

--- StudyApp/StudyApp/Assets/Models/File.cs
+++ StudyApp/StudyApp/Assets/Models/File.cs
@@ -10,5 +10,7 @@ namespace StudyApp.Assets.Models
         public string Name { get; set; }
         public Dictionary<string, Permission> Users { get; set; }
         public byte[] Content { get; set; }
+
+        public static implicit operator FileMini(File f) => new FileMini { GUID = f.GUID, Name = f.Name, Extension = f.Extension, Size = f.Content.Length };
     }
 }
