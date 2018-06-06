b6c663cff308a43cfa3887fc8ed4f368bc5c4ac3
Ramon Caballero <ramon.caballeroiv@gmail.com>
2018-06-05 23:52:43 -0600

    Final commit before submission

--- StudyApp/StudyApp/Assets/Controllers/SerializationBinderHelper.cs
+++ StudyApp/StudyApp/Assets/Controllers/SerializationBinderHelper.cs
@@ -16,12 +16,34 @@ using Newtonsoft.Json.Serialization;
 
 namespace StudyApp.Assets.Controllers {
 
+    internal class ByteArrayConverter : JsonConverter {
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+            string base64String = Convert.ToBase64String((byte[]) value);
+
+            serializer.Serialize(writer, base64String);
+        }
+
+        public override bool CanRead {
+            get { return false; }
+        }
+
+        public override bool CanConvert(Type t) {
+            return typeof(byte[]).IsAssignableFrom(t);
+        }
+    }
+
     internal class SerializationBinderHelper : ISerializationBinder {
 
         public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings {
             TypeNameHandling = TypeNameHandling.Objects,
             Formatting = Formatting.Indented,
-            SerializationBinder = new SerializationBinderHelper()
+            SerializationBinder = new SerializationBinderHelper(),
+            Converters = { new ByteArrayConverter() }
         };
 
         public IList<Type> KnownTypes { get; set; }
